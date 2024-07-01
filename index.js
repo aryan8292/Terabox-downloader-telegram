41ac497ef8395e2fb8802bd6822fdf75cc479e75
aryan8292 <94910943+aryan8292@users.noreply.github.com>
2024-07-02 01:09:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = 'YOUR_BOT_TOKEN'; // Replace with your actual bot token
-const bot = new TelegramBot(token, { polling: true });
 
-const GROUP_ID = -4226889924; // Your group ID
-const GROUP_LINK = 'https://t.me/+U3TzOtic2x4zYzc9'; // Your group link
+ async function main() {
+  const { Telegraf, Markup } = require("telegraf");
+  const { getDetails } = require("./api");
+  const { sendFile } = require("./utils");
+  const express = require("express");
 
-bot.on('message', (msg) => {
-    if (msg.chat.id === GROUP_ID) {
-        // Handle messages from the specific group
-        bot.sendMessage(msg.chat.id, 'Hello, group!');
-    } else if (msg.chat.type === 'private') {
-        // Handle private messages
-        bot.sendMessage(msg.chat.id, `To use this bot, please join our group: ${GROUP_LINK}`);
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+
+  bot.start(async (ctx) => {
+    try {
+      ctx.reply(
+        Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.,
+        Markup.inlineKeyboard([
+          Markup.button.url(" ✅Channel", "https://t.me/ary_botz"),
+          Markup.button.url("⚠️Report bug", "https://t.me/Ary_botz"),
+        ]),
+      );
+    } catch (e) {
+      console.error(e);
     }
-});
+  });
+
+  bot.on("message", async (ctx) => {
+    if (ctx.message && ctx.message.text) {
+      const messageText = ctx.message.text;
+      if (
+        messageText.includes("terabox.com") ||
+        messageText.includes("teraboxapp.com")
+      ) {
+        //const parts = messageText.split("/");
+        //const linkID = parts[parts.length - 1];
+
+        // ctx.reply(linkID)
+
+        const details = await getDetails(messageText);
+        if (details && details.direct_link) {
+          try {
+            ctx.reply(Sending Files Please Wait.!!);
+            sendFile(details.direct_link, ctx);
+          } catch (e) {
+            console.error(e); // Log the error for debugging
+          }
+        } else {
+          ctx.reply('Something went wrong 🙃');
+        }
+        console.log(details);
+      } else {
+        ctx.reply("Please send a valid Terabox link.");
+      }
+    } else {
+      //ctx.reply("No message text found.");
+    }
+  });
+
+  const app = express();
+  // Set the bot API endpoint
+  app.use(await bot.createWebhook({ domain: process.env.WEBHOOK_URL }));
+
+  app.listen(process.env.PORT || 3000, () => console.log("Server Started"));
+}
+
+main()
